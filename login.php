762b63721b02289292ee8bd9548f675ce0462a5d
Tekalign Getachew <tekaligngetachew114@gmail.com>
2024-10-05 10:18:22 -0700

    changed database crediential

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
-
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","sis");
+$con=mysqli_connect("fdb1029.awardspace.net", "4536066_sis", "@#abcABC123", "4536066_sis");
 if(!$con)
 {
     echo "connection failed";
@@ -52,15 +51,20 @@ if(isset($_POST['submit']))
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&amp;display=swap">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&amp;display=swap">
     <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
@@ -80,26 +84,28 @@ if(isset($_POST['submit']))
             </div>
             <div class="col-lg-4 bg-dark round">
                 <h1 class="text-center font-weight-bold">Login</h1>
-              
 
-				<table class="table text-white">
-				<form action="#" method="POST">
-                    <tr>
-                        <td>Username: </td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" name="username" class="form-control" placeholder="Enter username.."></td>
-                    </tr>
-                    <tr>
-                        <td>Password: </td>
-                    </tr>
-                    <tr>
-                        <td><input type="password"  name="password" class="form-control" placeholder="Enter your password..."></td>
-                    </tr>
-                    <tr class="text-center">
-						<td><button name="submit" class="btn btn-secondary">Login</button></td>
-                    </tr>
-				</form>
+
+                <table class="table text-white">
+                    <form action="#" method="POST">
+                        <tr>
+                            <td>Username: </td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="username" class="form-control" placeholder="Enter username..">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Password: </td>
+                        </tr>
+                        <tr>
+                            <td><input type="password" name="password" class="form-control"
+                                    placeholder="Enter your password..."></td>
+                        </tr>
+                        <tr class="text-center">
+                            <td><button name="submit" class="btn btn-secondary">Login</button></td>
+                        </tr>
+                    </form>
                     <tr>
                         <td>
                             &nbsp;&nbsp;&nbsp;
